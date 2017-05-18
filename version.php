bd302e10d93554eb72a5348388379459e7646c68
ridwanp7 <ridwanp7@gmail.com>
2017-05-18 21:50:00 +0700

    Update version.php
    $plugin->component = 'local_wstemplate';

--- version.php
+++ version.php
@@ -19,6 +19,7 @@ $plugin->version  = 2011101202;   // The (date) version of this module + 2 extra
                                   // need then a $plugin->release human friendly date. For the moment, we use
                                   // display this version number with userdate (dev friendly)
 $plugin->requires = 2010112400;  // Requires this Moodle version - at least 2.0
+$plugin->component = 'local_wstemplate';
 $plugin->cron     = 0;
 $plugin->release = '1.0 (Build: 2011101202)';
 $plugin->maturity = MATURITY_STABLE;
