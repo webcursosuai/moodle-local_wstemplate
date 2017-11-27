6a854ae08f3104799cbbb985e127782d0a049e6b
Mihail Pozarski <mpozarski944@gmail.com>
2017-11-27 09:51:22 -0300

    testing json and array

--- externallib.php
+++ externallib.php
@@ -58,7 +58,7 @@ class local_wstemplate_external extends external_api {
         }
         $return = $DB->get_record_sql('SELECT * FROM mdl_course WHERE id = 10');
 
-        return $params['welcomemessage'] . $USER->firstname ." ". $return->fullname;
+        return array($params['welcomemessage'] . $USER->firstname ." ". $return->fullname);
     }
 
     /**
