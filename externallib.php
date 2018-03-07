56060d0c1a7ce0812a2da1050207c826d9cf05bc
webcursosti <webcursosti@10.50.129.82>
2018-03-07 15:29:10 -0300

    3th webservice service

--- externallib.php
+++ externallib.php
@@ -66,7 +66,7 @@ class local_webservice_external extends external_api {
                                                 GROUP BY f.id', array($courseid,"feedback"));
                 break;
             case($courseid > 0 && $feedbackid >0):
-                $return = $DB->get_records_sql('SELECT c.id, f.id, max(fc.timemodified) FROM {course} AS c
+                $return = $DB->get_records_sql('SELECT c.id, f.id, f.name, fi.id, fi.name, fi.presentation, fi.typ, fv.id, fv.value FROM {course} AS c
                                                 INNER JOIN {course_modules} AS cm ON (c.id = cm.course AND c.id = ?)
                                                 INNER JOIN {modules} AS m ON (cm.module = m.id AND m.name = ?)
                                                 INNER JOIN {feedback} AS f ON (c.id = f.course AND f.id = ?)
