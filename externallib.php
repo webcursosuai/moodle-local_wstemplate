080c5ccee3ea76e483e722ae37c816cf4dd13e79
webcursosti <webcursosti@webcursoss-imac.uai.cl>
2018-03-29 12:21:35 -0300

    debug

--- externallib.php
+++ externallib.php
@@ -98,7 +98,7 @@ class local_webservice_external extends external_api {
                         $return->responses = $responses;
                         $result[] = $return;
                     }
-                    if($question->type_id == 8){
+                   /* if($question->type_id == 8){
                         $rankquestions = $DB->get_records_sql('SELECT id, content FROM {questionnaire_quest_choice} WHERE question_id = ?', array($question->id));
                         foreach($rankquestions as $rank){
                             $responses = $DB->get_records_sql('SELECT id, rank+1 FROM {questionnaire_response_rank} WHERE choice_id = ?', array($rank->id));
@@ -106,7 +106,7 @@ class local_webservice_external extends external_api {
                             $return->responses = $responses;
                             $result[] = $return;
                         }
-                    }
+                    }*/
                 }
                 
                 
