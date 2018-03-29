663e02a93146bf1d64975940a66da80307798160
webcursosti <webcursosti@webcursoss-imac.uai.cl>
2018-03-29 12:39:05 -0300

    debug

--- externallib.php
+++ externallib.php
@@ -91,13 +91,14 @@ class local_webservice_external extends external_api {
                 $questions = $DB->get_records_sql('SELECT id, name,content, type_id, length, position
                                                     FROM {questionnaire_question}
                                                     WHERE survey_id = ? order by position',array($feedbackid));
-                $result->query = $questions;
+                
                 foreach($questions as $question){
                     if($question->type_id == 2 || $question->type_id == 3 || $question->type_id == 10){
                         $responses = $DB->get_record_sql('SELECT id, response FROM {questionnaire_response_text} WHERE question_id = ?', array($question->id));
                         $result->category = $question->name;
                         $result->question = $question->content;
                         $result->responses = $responses;
+                        $result->query = $responses;
                         $return[] = $result;
                     }
                     if($question->type_id == 8){
